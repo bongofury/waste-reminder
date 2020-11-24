8b6d75667805fef6df493c72edf37b4bab82e90e
bongofury <edotesi@gmail.com>
2020-11-24 10:56:39 +0100

    Fix lambda

--- lambda-functions/testDate.js
+++ lambda-functions/testDate.js
@@ -1,6 +1,9 @@
-exports.handler = () => ({
-  statusCode: 200,
-  body: {
-    current: new Date().toISOString(),
-  },
-});
+exports.handler = async (e) => {
+  console.log('hit testDate fnc alive', e);
+  return {
+    statusCode: 200,
+    body: {
+      current: new Date().toISOString(),
+    },
+  };
+};
