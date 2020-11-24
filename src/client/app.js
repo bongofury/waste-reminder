6323f7152de167c5d1e36c829aa56d1ccdb4861f
bongofury <edotesi@gmail.com>
2020-11-24 12:05:54 +0100

    Fix front end

--- src/client/app.js
+++ src/client/app.js
@@ -5,14 +5,12 @@
       .then((res) => res.json());
     const { items, date } = garbageResponse;
     mainEl.innerHTML = items.length
-      ? `
-        <p>Domani (giorno ${date}) ritirano:
+      ? `<p>Domani (giorno ${date}) ritirano:
           <ul>
-            ${items.map((i) => `<li>${i}</li>`)}
+            ${(items.map((i) => `<li>${i}</li>`)).join('')}
           </ul>
-        </p>
-      `
-      : `<p>Domani (giorno ${date}) non si ritira la spazzatura`;
+        </p>`
+      : `<p>Domani (giorno ${date}) non si ritira la spazzatura</p>`;
   } catch (e) {
     mainEl.innerHTML = `
       <p>Errore! ${e}</p>
