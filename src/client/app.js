0932c922f0b3c1a593b51f817ff489dcf8d44d5e
bongofury <edotesi@gmail.com>
2020-11-24 11:52:22 +0100

    Add primitive front end

--- src/client/app.js
+++ src/client/app.js
@@ -1,10 +1,19 @@
-(() => {
-  const btn = document.getElementById('notification-on');
-  btn.addEventListener('click', () => {
-    Notification.requestPermission().then((perm) => {
-      if (perm === 'granted') {
-        new Notification('Ciao!');
-      }
-    });
-  });
+(async () => {
+  const mainEl = document.getElementsByTagName('main')[0];
+  try {
+    const garbageResponse = await window.fetch('/.netlify/functions/garbage')
+      .then((res) => res.json());
+    const { items, date } = garbageResponse;
+    mainEl.innerHTML = `
+      <p>Domani (giorno ${date}) ritirano:
+        <ul>
+          ${items.map((i) => `<li>${i}</li>`)}
+        </ul>
+      </p>
+    `;
+  } catch (e) {
+    mainEl.innerHTML = `
+      <p>Errore! ${e}</p>
+    `;
+  }
 })();
